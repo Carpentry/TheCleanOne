9751b7bf241ef41e38eb8f4411f08ae53a521151
lihp <lihp@lihpdeMacBook-Pro.local>
2015-06-23 14:23:38 +0800

    Initial

---
+++ TheCleanOne/TheCleanOne/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TheCleanOne
+//
+//  Created by lihp on 15/6/23.
+//  Copyright (c) 2015年 liyuelong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TheCleanOne/TheCleanOne/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TheCleanOne
+//
+//  Created by lihp on 15/6/23.
+//  Copyright (c) 2015年 liyuelong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
